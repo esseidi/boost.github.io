02f0791f4f31c0ed55191e5583749ff7506f53dc
Anas esseidi <anas.essaidi@ext.cdiscount.com>
2019-06-10 12:46:21 +0100

    Updates

---
+++ precache-manifest.2f2d113930cd3639642627adad71580a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa1f99dc4c40e5a082e31222a2f2436e",
+    "url": "/home/static/media/AraJozoor-Regular.fa1f99dc.eot"
+  },
+  {
+    "revision": "21e0ea2805e0c6a8ecb2",
+    "url": "/home/static/css/main.9b313a87.chunk.css"
+  },
+  {
+    "revision": "4e1ec8947a6857600002",
+    "url": "/home/static/js/runtime~main.4e1ec894.js"
+  },
+  {
+    "revision": "5a43dec2b91f6cbb4d6d62dfb83da50a",
+    "url": "/home/static/media/009-cloudy-night.5a43dec2.svg"
+  },
+  {
+    "revision": "72abacdd52d5ef320529",
+    "url": "/home/static/js/2.72abacdd.chunk.js"
+  },
+  {
+    "revision": "289cda261d2a7d3e6e853b30e16d718a",
+    "url": "/home/static/media/AraJozoor-Regular.289cda26.ttf"
+  },
+  {
+    "revision": "f2b8b184ba39f851059aeada97db517b",
+    "url": "/home/static/media/AraJozoor-Regular.f2b8b184.svg"
+  },
+  {
+    "revision": "b573015b8452e0f4eb977bbfb193f03f",
+    "url": "/home/static/media/AraJozoor-Regular.b573015b.woff"
+  },
+  {
+    "revision": "21e0ea2805e0c6a8ecb2",
+    "url": "/home/static/js/main.21e0ea28.chunk.js"
+  },
+  {
+    "revision": "28157ec23ba997350570797d304ca708",
+    "url": "/home/static/media/016-cloudy-night-3.28157ec2.svg"
+  },
+  {
+    "revision": "1f69a3e9e85c2ed02d73bfc813c0efec",
+    "url": "/home/static/media/002-sunrise.1f69a3e9.svg"
+  },
+  {
+    "revision": "004ec82a40d4fadb92b08d52fc464dcb",
+    "url": "/home/static/media/001-sun.004ec82a.svg"
+  },
+  {
+    "revision": "aa8cc3774681f3f899e2ac4105fac807",
+    "url": "/home/static/media/015-cloudy-day-1.aa8cc377.svg"
+  },
+  {
+    "revision": "53a38aace3bcc6b79631ab4755ceba72",
+    "url": "/home/static/media/003-sunset.53a38aac.svg"
+  },
+  {
+    "revision": "72abacdd52d5ef320529",
+    "url": "/home/static/css/2.f24519e3.chunk.css"
+  },
+  {
+    "revision": "e76a3b37dcf0c390435cd29cb49a5753",
+    "url": "/home/index.html"
+  }
+];
